62083bbd2caef6b6714a89673e48731c29d09238
amcspps <pashafizmat1@gmail.com>
2024-03-24 02:12:31 +0300

    lab-2 initial

---
+++ Window/vertex-shader.hlsl
@@ -0,0 +1,23 @@
+struct VSInput 
+{
+	float3 pos : POSITION;
+	float4 color : COLOR;
+};
+
+struct VSOutput 
+{
+	float4 pos : SV_POSITION;
+	float4 color : COLOR;
+};
+
+
+VSOutput vs(VSInput vertex)
+{
+    VSOutput result;
+    
+	result.pos = float4(vertex.pos, 1.0);
+    result.color = vertex.color;
+	
+    return result;
+
+}
